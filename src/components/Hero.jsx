d9adf44bf509bc77dcdc6372c8628878e90feb50
Rimaz Khan <rimazk@umich.edu>
2021-02-19 16:53:51 -0500

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../css/Hero.css"
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-container">
+        <h2>Hero</h2>
+      </div>
+    </div>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,9 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import Hero from "./Hero"
+import "../css/layout.css"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,6 +28,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <Hero />
       <div
         style={{
           margin: `0 auto`,
